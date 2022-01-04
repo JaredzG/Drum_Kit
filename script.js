4894859a33c80686242b8ccad7aaa9460ff22eb2
Jared <jaredzgallardo@gmail.com>
2022-01-03 21:43:24 -0600

    reduce sound playback code, add sound highlights

--- script.js
+++ script.js
@@ -1,104 +1,37 @@
-let boom = document.getElementById('boom-sound');
-let clap = document.getElementById('clap-sound');
-let hihat = document.getElementById('hihat-sound');
-let kick = document.getElementById('kick-sound');
-let openhat = document.getElementById('openhat-sound');
-let ride = document.getElementById('ride-sound');
-let snare = document.getElementById('snare-sound');
-let tink = document.getElementById('tink-sound');
-let lowtom = document.getElementById('lowtom-sound');
-let hightom = document.getElementById('hightom-sound');
+// Sounds that will play upon keypress
+let audioSounds = document.querySelectorAll('audio');
 
 document.addEventListener('keypress', playSound);
 
+// Plays appropriate sound after calling stopSound
 function playSound(event) {
-    stopSound(event.key);
-    switch(event.key) {
-        case 'a' :
-            boom.play();
-            break;
-        case 's' :
-            clap.play();
-            break;
-        case 'd' :
-            hihat.play();
-            break;
-        case 'f' :
-            kick.play();
-            break;
-        case 'g' :
-            openhat.play();
-            break;
-        case 'h' :
-            ride.play();
-            break;
-        case 'j' :
-            snare.play();
-            break;
-        case 'k' :
-            tink.play();
-            break;
-        case 'l' :
-            lowtom.play();
-            break;
-        case ';' :
-            hightom.play();
-            break;
-    }
+    stopSound(event);
+    for (const audioSound of audioSounds)
+        if (event.key === audioSound.previousElementSibling.previousElementSibling.textContent.toLowerCase())
+            audioSound.play();
 }
 
-function stopSound(key) {
-    switch(key) {
-        case 'a' :
-            if (!boom.paused && boom.currentTime) {
-                boom.pause();
-                boom.currentTime = 0;
+// Stops and resets a sound that is currently playing
+function stopSound(event) {
+    for (const audioSound of audioSounds)
+        if (event.key === audioSound.previousElementSibling.previousElementSibling.textContent.toLowerCase())
+            if (!audioSound.paused && audioSound.currentTime) {
+                audioSound.pause();
+                audioSound.currentTime = 0;
             }
-            break;
-        case 's' :
-            if (!clap.paused && clap.currentTime) {
-                clap.pause();
-                clap.currentTime = 0;
-            }
-        case 'd' :
-            if (!hihat.paused && hihat.currentTime) {
-                hihat.pause();
-                hihat.currentTime = 0;
-            }
-        case 'f' :
-            if (!kick.paused && kick.currentTime) {
-                kick.pause();
-                kick.currentTime = 0;
-            }
-        case 'g' :
-            if (!openhat.paused && openhat.currentTime) {
-                openhat.pause();
-                openhat.currentTime = 0;
-            }
-        case 'h' :
-            if (!ride.paused && ride.currentTime) {
-                ride.pause();
-                ride.currentTime = 0;
-            }
-        case 'j' :
-            if (!snare.paused && snare.currentTime) {
-                snare.pause();
-                snare.currentTime = 0;
-            }
-        case 'k' :
-            if (!tink.paused && tink.currentTime) {
-                tink.pause();
-                tink.currentTime = 0;
-            }
-        case 'l' :
-            if (!lowtom.paused && lowtom.currentTime) {
-                lowtom.pause();
-                lowtom.currentTime = 0;
-            }
-        case ';' :
-            if (!hightom.paused && hightom.currentTime) {
-                hightom.pause();
-                hightom.currentTime = 0;
-            }
+}
+
+// Divs that will be highlighted when their sound plays
+let soundDivs = document.querySelectorAll('.play-sound');
+
+document.addEventListener('keydown', applyPlayingClass);
+document.addEventListener('keyup', applyPlayingClass);
+
+// Toggles the 'playing' class for special CSS rules when a sound plays
+function applyPlayingClass(event) {
+    for (const soundDiv of soundDivs) {
+        if (event.key === soundDiv.firstElementChild.textContent.toLowerCase()) {
+            soundDiv.classList.toggle('playing');
+        }
     }
 }
